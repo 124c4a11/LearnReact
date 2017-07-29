35dff69682f327c30aaeebf9b097d8843a4814e0
124c4a11 <124c4a11@mail.ru>
2017-07-29 19:19:57 +0300

    index.js: Import articles from fixtures.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import Article from './Article';
 import ArticleList from './ArticleList';
-import {normalizedArticles as articles} from './fixtures';
+import {articles} from './fixtures';
 
 render(<ArticleList articles = {articles} />, document.getElementById('container'));
