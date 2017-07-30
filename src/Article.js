478f73b4d71a3dccfbba6f8e22d13e8ba5db428f
124c4a11 <124c4a11@mail.ru>
2017-07-30 16:33:16 +0300

    add CommentsList

--- src/Article.js
+++ src/Article.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import CommentsList from './CommentsList';
 
 
 export default class Article extends Component {
@@ -28,7 +29,12 @@ export default class Article extends Component {
   getBody() {
     if (!this.state.isOpen) return null;
     const {article} = this.props;
-    return <section>{article.text}</section>;
+    return (
+      <section>
+        {article.text}
+        <CommentsList comments = {article.comments}/>
+      </section>
+    );
   };
 
   toggleOpen = () => {
---
+++ src/CommentsList.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Comment from './Comment';
+
+export default class CommentsList extends Component {
+  state = {
+    isOpen: false
+  };
+
+  render() {
+    const {isOpen} = this.state;
+
+    return (
+      <div>
+        <button onClick = {this.toggleOpen}>
+          {isOpen ? 'Close' : 'Open'}
+        </button>
+        {this.getBody()}
+      </div>
+    );
+  };
+
+  getBody() {
+    if(!this.state.isOpen) return null;
+
+    const {comments} = this.props;
+    if (!comments || !comments.length) return <p>no comments</p>
+
+    return (
+      <ul>
+        {comments.map((comment) => <li key = {comment.id}><Comment comment = {comment}/></li>)}
+      </ul>
+    );
+  };
+
+  toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+}
