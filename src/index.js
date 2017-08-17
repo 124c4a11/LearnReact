ce48e13375843eedb42f261d06b9f0e3982eb4f0
124c4a11 <124c4a11@mail.ru>
2017-08-17 15:43:02 +0300

    CLEAR

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Article from './components/Article';
-import Root from './components/Root';
-import store from './store';
-
-
-render(<Root />, document.getElementById('container'));
