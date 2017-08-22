054797635559e9d1f55df06f9d1c68d4e32be394
124c4a11 <124c4a11@mail.ru>
2017-08-22 17:15:38 +0300

    add accordion decorator

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
 import Article from './Article';
+import accordion from '../decorators/accordion';
 
 
-export default class ArticleList extends Component {
-  state = {
-    openArticleId: null
-  };
-
+class ArticleList extends Component {
   render() {
-    const articleElements = this.props.articles.map((article) => <li key={article.id}>
+    const {articles, isOpen, openItemId, toggleOpenItem} = this.props;
+    const articleElements = articles.map((article) => <li key={article.id}>
       <Article
         article={article}
-        isOpen={article.id === this.state.openArticleId}
-        toggleOpen={this.toggleOpenArticle(article.id)}
+        isOpen={article.id === openItemId}
+        toggleOpen={toggleOpenItem(article.id)}
       />
     </li>);
 
@@ -22,8 +20,7 @@ export default class ArticleList extends Component {
       </ul>
     );
   }
-
-  toggleOpenArticle = (openArticleId) => (e) => {
-    this.setState({ openArticleId });
-  };
 }
+
+
+export default accordion(ArticleList);
---
+++ src/decorators/accordion.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+export default (OriginalComponent) => class Accordion extends Component {
+  state = {
+    openItemId: null
+  };
+
+  render() {
+    return (
+      <OriginalComponent
+        {...this.props}
+        openItemId={this.state.openItemId}
+        toggleOpenItem={this.toggleOpenItem}
+      />
+    );
+  }
+
+  toggleOpenItem = (itemId) => (e) => {
+    this.setState({
+      openItemId: itemId === this.state.openItemId ? null : itemId
+    });
+  };
+}
