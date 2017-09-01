1d79b3c1d608093b9bcee43221525913b0986675
124c4a11 <124c4a11@mail.ru>
2017-09-01 16:50:56 +0300

    add normalized data

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 
-export default function Comment({comment}) {
+function Comment({comment}) {
   return (
     <div>
       <p>{comment.text} <b>by {comment.user}</b></p>
@@ -11,8 +12,17 @@ export default function Comment({comment}) {
 }
 
 Comment.propTypes = {
+  id: PropTypes.string.isRequired,
+  // from connect
   comment: PropTypes.shape({
     text: PropTypes.string.isRequired,
     user: PropTypes.string
   }).isRequired
 };
+
+
+export default connect((state, ownProps) => {
+  return {
+    comment: state.comments.find((comment) => comment.id === ownProps.id)
+  };
+})(Comment);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import counteReducer from './counter';
 import articles from './articles';
 import filters from './filters';
+import comments from './comments';
 
 
 export default combineReducers({
   count: counteReducer,
   articles,
-  filters
+  filters,
+  comments
 });
