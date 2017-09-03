fb0e0c9793a69106f95812bebf2aa288d20ed7f7
124c4a11 <124c4a11@mail.ru>
2017-09-03 14:18:24 +0300

    add filtratedArticlesSelector

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,6 +3,7 @@ import Article from './Article';
 import PropTypes from 'prop-types';
 import accordion from '../decorators/accordion';
 import {connect} from 'react-redux';
+import {filtratedArticlesSelector} from '../selectors';
 
 
 class ArticleList extends Component {
@@ -33,16 +34,8 @@ class ArticleList extends Component {
 }
 
 
-export default connect(({filters, articles}) => {
-  const {selected, dateRange: {from, to}} = filters;
-
-  const filteredArticles = articles.filter((article) => {
-    const published = Date.parse(article.date);
-    
-    return (!selected.length || selected.includes(article.id) && (!from || !to || (published > from && published < to)));
-  });
-
+export default connect((state) => {
   return {
-    articles: filteredArticles
+    articles: filtratedArticlesSelector(state)
   };
 })(accordion(ArticleList));
