1db3576d81ae8ce12cd9618638837b3eb3f3dc93
124c4a11 <124c4a11@mail.ru>
2017-09-05 15:14:24 +0300

    add commentSelector

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {commentSelector} from '../selectors';
 
 
 function Comment({comment}) {
@@ -22,5 +23,5 @@ Comment.propTypes = {
 
 
 export default connect((state, ownProps) => ({
-  comment: state.comments.find((comment) => comment.id === ownProps.id)
+  comment: commentSelector(state, ownProps)
 }))(Comment);
