88e4b5b192c94bd89c5247f0fac2a08449986c56
124c4a11 <124c4a11@mail.ru>
2017-09-19 12:35:09 +0300

    add Loader

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import Article from './Article';
 import PropTypes from 'prop-types';
 import accordion from '../decorators/accordion';
 import {connect} from 'react-redux';
 import {filtratedArticlesSelector} from '../selectors';
 import {loadAllArticles} from '../AC';
+
+import Article from './Article';
+import Loader from './Loader';
 
 
 class ArticleList extends Component {
@@ -17,11 +19,15 @@ class ArticleList extends Component {
   };
 
   componentDidMount() {
-    this.props.loadAllArticles();
+    const {loaded, loading, loadAllArticles} = this.props;
+    if (!loaded || !loading) this.props.loadAllArticles();
   }
 
   render() {
-    const {articles, isOpen, openItemId, toggleOpenItem} = this.props;
+    const {articles, isOpen, openItemId, toggleOpenItem, loading} = this.props;
+
+    if (loading) return <Loader />;
+
     const articleElements = articles.map((article) => <li key={article.id}>
       <Article
         article={article}
@@ -41,6 +47,8 @@ class ArticleList extends Component {
 
 export default connect((state) => {
   return {
-    articles: filtratedArticlesSelector(state)
+    articles: filtratedArticlesSelector(state),
+    loading: state.articles.loading,
+    loaded: state.articles.loaded
   };
 }, {loadAllArticles})(accordion(ArticleList));
--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,5 +1,5 @@
 import {normalizedArticles as defaultArticles} from '../fixtures';
-import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES} from '../constants';
+import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES, START, SUCCESS} from '../constants';
 import {arrToMap} from '../helpers';
 import {OrderedMap, Map, Record} from 'immutable';
 
@@ -26,8 +26,14 @@ export default (articleState = defaultState, action) => {
   const {type, payload, randomId, response} = action;
 
   switch (type) {
-    case LOAD_ALL_ARTICLES:
-      return articleState.set('entities', arrToMap(response, ArticleRecord));
+    case LOAD_ALL_ARTICLES + START:
+      return articleState.set('loading', true);
+
+    case LOAD_ALL_ARTICLES + SUCCESS:
+      return articleState
+        .set('entities', arrToMap(response, ArticleRecord))
+        .set('loading', false)
+        .set('loaded', true)
 
     case DELETE_ARTICLE:
       return articleState.deleteIn(['entities', payload.id]);
